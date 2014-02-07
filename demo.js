327708f59bdecb2bb7195bce35b1061dd0d3d634
deathcap <deathcap@gmx.co.uk>
2014-02-07 12:49:03 -0800

    Add filesystem: URLs. Closes GH-1

--- demo.js
+++ demo.js
@@ -1,6 +1,10 @@
 var url4data = require('./');
 
 console.log(url4data('foo','bar',{type:'text/javascript'}, function(url) { 
-  console.log('url4data = ',url);
+  console.log('url4data blob = ',url);
+}));
+
+console.log(url4data('foo','bar',{type:'text/javascript', scheme:'filesystem'}, function(url) { 
+  console.log('url4data filesystem = ',url);
 }));
 
--- index.js
+++ index.js
@@ -25,6 +25,8 @@ var createNewBlobURL = function(data, name, opts) {
 };
 
 var blobURL4data = function(data, name, opts, cb) {
+  if (!window.Blob) return null; // unsupported
+
   var url = window.localStorage[name];
 
   isValidBlobURL(url, function(url, isValid) {
@@ -38,9 +40,48 @@ var blobURL4data = function(data, name, opts, cb) {
   });
 }
 
+var createNewFilesystemURL = function(data, name, opts, cb) {
+  if (!window.webkitRequestFileSystem) return null; // unsupported
+
+  var size = data.length;
+
+  window.webkitRequestFileSystem(window.TEMPORARY, size, function(fs) {
+    fs.root.getFile(name, {create:true}, function(file) {
+      console.log('created file',file);
+      file.createWriter(function(writer) {
+        writer.onwriteend = function() {
+          console.log('File write completed');
+        };
+
+        writer.onerror = function(err) {
+          console.log('createWriter error:',err);
+        };
+
+        var blob = new Blob([data], opts); // we can only write blobs?
+        writer.write(blob);
+
+        var url = file.toURL();
+        cb(url, data, name, opts);
+      });
+    }, function(err) {
+      console.log('getFile error:',err);
+    });
+  }, function(err) {
+    console.log('webkitRequestFileSystem error:',err);
+  });
+};
+
+var filesystemURL4data = function(data, name, opts, cb) {
+  return createNewFilesystemURL(data, name, opts, cb); // TODO: reuse existing
+}
 
 var url4data = function(data, name, opts, cb) {
-  return blobURL4data(data, name, opts, cb); // TODO: other types. filesystem:...
+  var scheme = opts.scheme || 'blob'; // TODO: multiple schemes, try available
+
+  if (scheme === 'blob')
+    return blobURL4data(data, name, opts, cb);
+  else if (scheme === 'filesystem')
+    return filesystemURL4data(data, name, opts, cb);
 };
 
 module.exports = url4data;
